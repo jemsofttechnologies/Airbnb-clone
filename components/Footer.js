395a85df8365760d8a7e43f991383debdaed377d
jemsofttechnologies <jemsofttechnologies@gmail.com>
2022-01-15 13:54:14 +0300

    index footer

---
+++ components/Footer.js
@@ -0,0 +1,41 @@
+const Footer = () => {
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-4 px-14 lg:px-48 py-14 gap-y-10 bg-gray-100 text-gray-600">
+			<div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+			<div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>Its a pretty awesome clone</p>
+                <p>Referrals accepted</p>
+                <p>Jemsoft</p>
+            </div>
+			<div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>Jemsoft Technologies</p>
+                <p>Presents</p>
+                <p>Airbnb Clone</p>
+                <p>For Education purposes</p>
+                <p>Join Me</p>
+            </div>
+			<div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Help Centre</p>
+                <p>Trust & Safety</p>
+                <p>24/7 support</p>
+                <p>Ready to help</p>
+                <p>At any time</p>
+            </div>
+			
+		</div>
+	);
+};
+
+export default Footer;
